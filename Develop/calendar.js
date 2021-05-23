04ea21b305ac5f3df091b72175f659af31d298e0
Rumtikitum <rt.student.only@gmail.com>
2021-05-23 08:15:37 -0700

    finished

--- Develop/calendar.js
+++ Develop/calendar.js
@@ -1,3 +1,6 @@
+myStorage = window.localStorage;
+
+
 var currentDay = document.querySelector("#currentDay")
 var nineam = document.querySelector("#nineam")
 var tenam = document.querySelector("#tenam")
@@ -165,3 +168,95 @@ if (currentTime == 5) {
 //----------------------------------------
 //End of time code------------------------
 //----------------------------------------
+//Save to Local Storage Portion-----------
+//----------------------------------------
+//Vars------------------------------------
+var savenine = document.querySelector("#savenine");
+var saveeleven = document.querySelector("#saveeleven");
+var saveten = document.querySelector("#saveten");
+var savetwelve = document.querySelector("#savetwelve");
+var saveone = document.querySelector("#saveone");
+var savetwo = document.querySelector("#savetwo");
+var savethree = document.querySelector("#savethree");
+var savefour = document.querySelector("#savefour");
+var savefive = document.querySelector("#savefive");
+
+//Local storage functions for each btn--------------
+savenine.addEventListener("click", function() {
+    console.log(nineam.value)
+    localStorage.setItem('ninevalue', nineam.value)
+});
+
+nineam.value = localStorage.getItem('ninevalue');
+
+//--------------------------------------------------
+
+saveten.addEventListener("click", function() {
+    console.log(tenam.value)
+    localStorage.setItem('tenvalue', tenam.value)
+})
+
+tenam.value = localStorage.getItem('tenvalue');
+
+//--------------------------------------------------
+
+saveeleven.addEventListener("click", function() {
+    console.log(elevenam.value)
+    localStorage.setItem('elevenvalue', elevenam.value)
+})
+
+elevenam.value = localStorage.getItem('elevenvalue');
+
+//----------------------------------------------------
+
+savetwelve.addEventListener("click", function() {
+    console.log(twelvepm.value)
+    localStorage.setItem('twelvevalue', twelvepm.value)
+})
+
+twelvepm.value = localStorage.getItem('twelvevalue');
+
+//----------------------------------------------------
+
+saveone.addEventListener("click", function() {
+    console.log(onepm.value)
+    localStorage.setItem('tenvalue', tenam.value)
+})
+
+onepm.value = localStorage.getItem('onevalue');
+
+//-------------------------------------------------------
+
+savetwo.addEventListener("click", function() {
+    console.log(twopm.value)
+    localStorage.setItem('twovalue', twopm.value)
+})
+
+twopm.value = localStorage.getItem('twovalue');
+
+//3PM----------------------------------------------------
+
+savethree.addEventListener("click", function() {
+    console.log(threepm.value)
+    localStorage.setItem('threevalue', threepm.value)
+})
+
+threepm.value = localStorage.getItem('threevalue');
+
+//4PM-----------------------------------------------------
+
+savefour.addEventListener("click", function() {
+    console.log(fourpm.value)
+    localStorage.setItem('fourvalue', fourpm.value)
+})
+
+fourpm.value = localStorage.getItem('fourvalue');
+
+//5PM------------------------------------------------------
+
+savefive.addEventListener("click", function() {
+    console.log(fivepm.value)
+    localStorage.setItem('fivevalue', fivepm.value)
+})
+
+fivepm.value = localStorage.getItem('fivevalue');
